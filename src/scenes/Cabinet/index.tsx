4c6683a633a6f8ee05d806cebccc421e6c567122
ISA002 <ggeasy1710@gmail.com>
2020-04-22 19:59:36 +0400

    move Auth component to scenes

--- src/scenes/Cabinet/index.tsx
+++ src/scenes/Cabinet/index.tsx
@@ -1,14 +1,23 @@
-import React from 'react'
+import React from "react";
 
-interface CabinetSceneProps {
+import Navbar from "../../components/Navbar";
+import { Layout, Col, Row, Card } from "antd";
+const { Content } = Layout;
 
-}
+interface CabinetSceneProps {}
 
 const CabinetScene = (props: CabinetSceneProps) => {
-    return (
-        <div>
-        </div>
-    )
-}
+  return (
+    <Layout className="CabinetScene layout h-100vh">
+      <Navbar />
+      <Content>
+        <Col className="pt-4" span="12" offset="6">
+          <Card bordered={false}>
+          </Card>
+        </Col>
+      </Content>
+    </Layout>
+  );
+};
 
-export default CabinetScene
+export default CabinetScene;
