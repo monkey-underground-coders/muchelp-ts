a84086b5f20e82171a7f2c689d02305e5d33fd17
senketsukamui <glebpotapov2001@gmail.com>
2020-04-22 20:52:39 +0300

    Make auth page UI

--- src/components/Application/index.tsx
+++ src/components/Application/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import GuestScene from "../../scenes/Guest";
 import CabinetScene from "../../scenes/Cabinet";
+import Auth from "../../scenes/Auth";
 
 const App = () => {
   const applicationRoutes = (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/" component={GuestScene} exact={true} />
       <Route path="/guest" component={GuestScene} />
       <Route path="/cabinet" component={CabinetScene} />
+      <Route path="/auth" component={Auth} />
     </Switch>
   )
 
--- src/scenes/Auth/index.tsx
+++ src/scenes/Auth/index.tsx
@@ -1,36 +1,50 @@
 import React from "react";
-import { Form, Checkbox, Button } from "antd";
+import { Form, Checkbox, Button, Row, Layout, Col } from "antd";
+import { GoogleOutlined } from '@ant-design/icons';
 import Title from "antd/lib/typography/Title";
 import Input from "antd/lib/input/Input";
-
+import "./index.scss";
+const { Content } = Layout;
 interface AuthProps {}
 const Auth = (props: AuthProps) => {
   return (
-    <>
-      <Form initialValues={{ remember: true }}>
-        <Title>Login</Title>
-        <Form.Item
-          label="Email"
-          name="email"
-          rules={[{ required: true, message: "Please input your email!" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item name="remember" valuePropName="checked">
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-        <Form.Item>
-          <Button type="primary">Submit</Button>
-        </Form.Item>
-      </Form>
-    </>
+    <Row>
+      <Layout className="h-100vh layout">
+        <Col className="pt-4" span="12" offset="6">
+          <Form>
+            <Title className = "text-center">Login</Title>
+            <Form.Item
+              label="Username"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your username!" },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                { required: true, message: "Please input your password!" },
+              ]}
+            >
+              <Input type="password" />
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" >
+                Submit
+              </Button>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" icon = {<GoogleOutlined />}>
+                Login using Google
+              </Button>
+            </Form.Item>
+          </Form>
+        </Col>
+      </Layout>
+    </Row>
   );
 };
 
